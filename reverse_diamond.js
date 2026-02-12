5f1e28ead9fbdc125f5af11db9cf6517d52de8a6
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-02-12 12:28:54 +0530

    Fix pattern generation in reverse diamond function

--- reverse_diamond.js
+++ reverse_diamond.js
@@ -4,7 +4,7 @@ for(let i=n; i>=1; i++) {
     pattren += " ".repeat(n-i) + "*".repeat(2*i-1)+ "\n";
 }
 for (let i=2; i<=n; i--){
-    pattren += " ".repeat(n-i) + "*".repeat(2*i-1) +"\n";
+    pattren += " ".repeat(n-i) + "*".repeat(2*i-1) +"\n"; 
 } 
 console.log(pattren);    
     
