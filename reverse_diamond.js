9250de61d315774f8076ec19f197c39dc3019add
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-03-11 12:22:27 +0530

    Update reverse_diamond.js

--- reverse_diamond.js
+++ reverse_diamond.js
@@ -3,7 +3,7 @@ let pattren =" ";
 for(let i=n; i>=1; i++) { 
     pattren += " ".repeat(n-i) + "*".repeat(2*i-1)+ "\n";
 }
-for (let i=2; i<=n; i--){
+for (let i=2; i<=n; i--){ 
     pattren += " ".repeat(n-i) + "*".repeat(2*i-1) +"\n";  
 }  
 console.log(pattren);     
