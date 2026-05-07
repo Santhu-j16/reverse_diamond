a404c9faeb562d50ddeaef1db5745f2f97203119
Santhosh Kumar J <santhujayanna459@gmail.com>
2026-05-07 05:34:01 +0530

    Fix loop condition in reverse diamond pattern

--- reverse_diamond.js
+++ reverse_diamond.js
@@ -1,7 +1,7 @@
 let n=5 ;     
 let pattren =" ";   
 for(let i=n; i>=1; i++) {    
-    pattren += " ".repeat(n-i) + "*".repeat(2*i-1)+ "\n";
+    pattren += " ".repeat(n-i) + "*".repeat(2*i-1)+ "\n"; 
 }
 for (let i=2; i<=n; i--){ 
     pattren += " ".repeat(n-i) + "*".repeat(2*i-1) +"\n";    
